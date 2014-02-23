0e8b8964961dbf9abfac179ffdded07c2fa92cb1
redguitarfreak <reachjohnnow@gmail.com>
2014-02-22 23:35:26 -0600

    Commented out a few things

--- app.rb
+++ app.rb
@@ -8,6 +8,20 @@ get '/' do
 end
 
 get '/page1' do
-  @things = ['Aye', 'Bee', 'Cee', 'Dee']
+  #@things = ['Aye', 'Bee', 'Cee', 'Dee']
   erb :'page1.html'
 end
+
+
+  # #<h2>My Tasks</h2>
+  # <ul>
+  #   <% @things.each do |thing| %>
+  #     <li><%= thing.to_s %></li>
+  #   <% end %>
+  #   </ul>
+
+  # <h2>My Goals </h2>
+  # <ul>
+  #   <li>Dig deeper</li>
+  #   <li>Say yes to less</li>
+  # </ul>
